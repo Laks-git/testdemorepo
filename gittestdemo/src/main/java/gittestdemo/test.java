407f1ea232c1a3ed775da4f49ceae22b861e89c7
Lucky <prasanna.kandregula.lpk@gmail.com>
2020-06-03 14:42:46 +0530

    First commit

---
+++ gittestdemo/src/main/java/gittestdemo/test.java
@@ -0,0 +1,11 @@
+package gittestdemo;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git Test1 Demo");
+		System.out.println("Git Test2 Demo");
+	}
+
+}
